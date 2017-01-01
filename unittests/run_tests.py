c3e35ddc357bc29fa9542e3c9baa6713b6595781
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2017-01-01 01:00:36 -0500

    updaed travis

--- unittests/run_tests.py
+++ unittests/run_tests.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
 	tests = [f for f in files if '.py' in f and 'run_tests.py' != f and 'test.py' != f]
 	for test in tests:
 		print 14*'*****'+'\nRunning: '+test+'\n'+14*'*****'+'\n'
-os.system('python '+test)
+		os.system('python '+test)
